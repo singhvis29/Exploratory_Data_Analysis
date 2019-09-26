a82b628ace6d7511cbb286a1bf5b60fb5fef311e
nithish k <you@example.com>
2019-09-26 15:08:53 -0400

    data changed

--- Mini-Project/Nithish.R
+++ Mini-Project/Nithish.R
@@ -1,18 +1,49 @@
 library(gapminder)
 library(tidyverse)
 
-myDf = gapminder
-
-lifeExpectancyDf = readxl::read_xlsx('indicator life_expectancy_at_birth.xlsx')
+gapminderDf = gapminder
 
 
+##load data
 
-lifeExpectancyDf = lifeExpectancyDf %>% 
-                    (function(x) {as.data.frame(t(x))})
+lifeExpectancyDf  = read.csv('life_expectancy_years.csv')
+gdpPerCapitaDf = read.csv('income_per_person_with_projections.csv' )
+
+
+## getinng continent mapping from gapminder df
+
+
+countryContinentMapping = gapminderDf %>% 
+  select(country,continent) %>% 
+  distinct(country,continent,.keep_all = TRUE)
+
+
+joinedDf_expectancy = inner_join(lifeExpectancyDf,countryContinentMapping)
+joinedDf_expectancy = joinedDf_expectancy %>% select(country,continent,everything())
 
 
 
-lifeExpectancyDf = lifeExpectancyDf %>% mutate(Year = row.names(lifeExpectancyDf))
-lifeExpectancyDf
 
-colnames(lifeExpectancyDf) = as.character(lifeExpectancyDf[1,])
+ExpectencyByContinent = joinedDf_expectancy %>% 
+  select(continent,"2016.0") %>% 
+  group_by(continent) %>% summarise(meanValues = mean(`2016.0`))
+
+# ggplot(ExpectencyByContinent, aes(x = Con, y = lifeExp, color = continent)) + geom_point(alpha = 0.3) + 
+#   scale_x_log10() + scale_color_manual(values = cb_palette) + 
+#   geom_smooth(method = "lm", se = FALSE)
+
+
+### gdp per capita data manips
+
+
+gdpPerCapitaDf = gdpPerCapitaDf %>% rename(country = "GDP per capita")
+
+joinedDf_gdp = gdpPerCapitaDf %>% 
+  inner_join(countryContinentMapping,by = c('country'='country'))
+
+
+GDPByContinent = joinedDf_gdp %>% 
+  select(continent,"2015.0") %>% 
+  group_by(continent) %>% summarise(meanValues = mean(`2015.0`))
+
+
