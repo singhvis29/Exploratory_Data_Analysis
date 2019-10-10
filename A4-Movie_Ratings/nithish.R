faeba7e24a6573465a15d15ea82031c8a4e9d783
nithish k <you@example.com>
2019-10-10 01:20:43 -0400

    added controur plot i think?

--- A4-Movie_Ratings/nithish.R
+++ A4-Movie_Ratings/nithish.R
@@ -37,11 +37,10 @@ selectedVars = RatingsWithBasics %>%
   mutate(cutYears = cut(startYear,5,dig.lab = 4))%>% 
   drop_na()
 
+
 outliers  =  boxplot(selectedVars$runtimeMinutes, plot=FALSE)$out
 
 selectedVars = selectedVars[-which(selectedVars$runtimeMinutes %in% outliers),]
-
-
 
 
 
@@ -55,6 +54,8 @@ summary(selectedVars)
 sum(is.na(selectedVars$averageRating))
 
 
+
+### linear model fitted
 lm(averageRating~ startYear+runtimeMinutes,data = transformed) %>%
   summary()
 
@@ -62,29 +63,53 @@ lm(averageRating~ startYear+runtimeMinutes,data = transformed) %>%
 
 
 #### Question 2
+library(broom)
+library(mgcv)
+
+modelGAM = gam(averageRating ~ s(startYear)+s(runtimeMinutes),data = selectedVars)
+summary(modelGAM)
 
 
-class(selectedVars$runtimeMinutes)
 
+
+predModelGAM = augment(modelGAM) %>% 
+  mutate(cutRuntimeMinutes = cut(runtimeMinutes,
+                                breaks = c(0,81,90,100,135),
+                                          dig.lab = 6)) %>% 
+  mutate(cutYears = cut(startYear,5,dig.lab = 4))
+
+
+
+##conditioned on years
 ggplot(selectedVars, aes(x = runtimeMinutes, y = averageRating)) + 
-  geom_point() + 
+  geom_point(size = 0.05) + 
   facet_grid(rows = vars(cutYears)) +
-  geom_smooth(method = "lm", se = FALSE)
+  geom_smooth(method = "gam", se = FALSE,span = 0.5)
 
 
-
+### conditioned on runtime
 ggplot(selectedVars, aes(x = startYear, y = averageRating)) + 
   geom_point() + 
   facet_grid(rows = vars(cutRuntimeMinutes)) +
-  geom_smooth(method = "lm", se = FALSE)+scale_x_log10()
+  geom_smooth(method = "gam", se = FALSE)
 
 
 
 
 
 
+#### Question 3 ####
+
+library(lattice)
+ggplot(predModelGAM, aes(x = startYear, y = runtimeMinutes, z = .fitted)) + 
+  geom_raster(aes(fill=.fitted)) + 
+  scale_fill_distiller(palette = "RdBu") +
+  geom_contour() + coord_fixed()
 
 
+
+wireframe(averageRating ~ startYear + runtimeMinutes,
+          data = selectedVars, drape = TRUE)
 
 
 
