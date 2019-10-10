73ee1140ff4435810590b4b7c99cf5ab1e947d76
nithish k <you@example.com>
2019-10-09 21:51:53 -0400

    added nihish file

--- A4-Movie_Ratings/nithish.R
+++ A4-Movie_Ratings/nithish.R
@@ -24,14 +24,65 @@ transformed = RatingsWithBasics %>%
   mutate(startYear = as.integer(startYear)) %>% 
   mutate(startYear = startYear-min(startYear,na.rm = TRUE)) %>% 
   # mutate_all(log) %>%
-  mutate_at(vars( -averageRating), funs(log(.))) %>%
+  mutate_at(vars( -c(averageRating,numVotes)), funs(log(.))) %>%
   drop_na() %>% 
   filter_all(all_vars(is.finite(.)))
 
 
 
+selectedVars = RatingsWithBasics %>% 
+  select(startYear,runtimeMinutes,numVotes,averageRating) %>% 
+  filter(runtimeMinutes>0) %>% 
+  mutate(startYear = as.integer(startYear)) %>% 
+  mutate(cutYears = cut(startYear,5,dig.lab = 4))%>% 
+  drop_na()
 
-lm(averageRating~ startYear+runtimeMinutes,data = transformed) %>% summary()
+outliers  =  boxplot(selectedVars$runtimeMinutes, plot=FALSE)$out
+
+selectedVars = selectedVars[-which(selectedVars$runtimeMinutes %in% outliers),]
+
+
+
+
+
+selectedVars = selectedVars %>% 
+              mutate(cutRuntimeMinutes = cut(runtimeMinutes,
+                        breaks = c(0,81,90,100,135),
+                        dig.lab = 6))
+summary(selectedVars)
+
+
+sum(is.na(selectedVars$averageRating))
+
+
+lm(averageRating~ startYear+runtimeMinutes,data = transformed) %>%
+  summary()
+
+
+
+
+#### Question 2
+
+
+class(selectedVars$runtimeMinutes)
+
+ggplot(selectedVars, aes(x = runtimeMinutes, y = averageRating)) + 
+  geom_point() + 
+  facet_grid(rows = vars(cutYears)) +
+  geom_smooth(method = "lm", se = FALSE)
+
+
+
+ggplot(selectedVars, aes(x = startYear, y = averageRating)) + 
+  geom_point() + 
+  facet_grid(rows = vars(cutRuntimeMinutes)) +
+  geom_smooth(method = "lm", se = FALSE)+scale_x_log10()
+
+
+
+
+
+
 
 
 
